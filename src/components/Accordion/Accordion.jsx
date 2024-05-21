dc3867277377b8f0a394fae3b9b1307c6356bd0c
habiburrahman0540 <habiburrahman0540@gmail.com>
2024-05-21 11:34:29 +0600

    revised accordion when click on one then another one will close

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import "./accordion.css"
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
-const Accordion = ({title,info}) => {
-  const [showInfo, setShowInfo] = useState(false);
+// Accordion.jsx
+import React from 'react';
+import "./accordion.css";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Accordion = ({ title, info, isOpen, toggleAccordion }) => {
   return (
     <article className='accordion_question'>
-        <header>
-            <h4>{title}</h4>
-            <button onClick={()=>setShowInfo(!showInfo)} className='accordion_btn'>
-              {showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}
-            </button>
-        </header>
-        {showInfo && <p>{info}</p>}
-        
+      <header>
+        <h4>{title}</h4>
+        <button onClick={toggleAccordion} className='accordion_btn'>
+          {isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
+      </header>
+      {isOpen && <p>{info}</p>}
     </article>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
--- src/components/Accordion/Accordions.jsx
+++ src/components/Accordion/Accordions.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react'
-import "./accordion.css"
-import data from "./data.js"
-import Accordion from './Accordion.jsx'
+// Accordions.jsx
+import React, { useState } from 'react';
+import "./accordion.css";
+import data from "./data.js";
+import Accordion from './Accordion.jsx';
+
 const Accordions = () => {
-  const [questions,setQuestions] = useState(data);
+  const [questions, setQuestions] = useState(data);
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setOpenIndex(index === openIndex ? null : index);
+  };
+
   return (
     <main className='accordion_main'>
-        <div className='accordion_container'>
-            <h3>Questions and answers about login</h3>
-            <section className='accordion_info'>
-                {questions.map((question)=>{
-                    return <Accordion key={question.id} {...question}></Accordion>
-                })}
-            </section>
-        </div>
+      <div className='accordion_container'>
+        <h3>Questions and answers about login</h3>
+        <section className='accordion_info'>
+          {questions.map((question, index) => {
+            return (
+              <Accordion
+                key={question.id}
+                isOpen={index === openIndex}
+                toggleAccordion={() => toggleAccordion(index)}
+                {...question}
+              />
+            );
+          })}
+        </section>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Accordions
+export default Accordions;
